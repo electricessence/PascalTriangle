038322c94ed61b725f2fd06a2b2204a0a8210089
electricessence <5899455+electricessence@users.noreply.github.com>
2020-01-08 12:35:48 -0800

    Updates.

--- Project Euler/ProjectEuler.Problem148/Program.cs
+++ Project Euler/ProjectEuler.Problem148/Program.cs
@@ -9,84 +9,85 @@ namespace ProjectEuler.Problem148
 	class Program
 	{
 		static readonly BigInteger SearchingFor = 7;
-		static readonly int MaxRow = 1_000_000_000;
-
+		static readonly ulong MaxRow = 1_000_000_000;
 		static void Main(string[] _)
 		{
 			BigInteger total = BigInteger.Zero;
 			//// Don't store 1s or row digits.
 			//// Setup first row that matters.
-			int foundCount = SearchingFor == 6 ? 1 : 0;
-			int rowIndex = 4;
-
 			var previousRow = new LinkedList<BigInteger>();
-			previousRow.AddLast(6);
+			previousRow.AddLast(BigInteger.One);
 
 			var nextRow = new LinkedList<BigInteger>();
 			nextRow.AddLast(BigInteger.Zero);
 			/////////////////////////////////
 
-		loop:
-
-			IncrementIfFound(rowIndex);
-			total += foundCount;
-			if (foundCount != 0 && rowIndex % 1000 == 0)
+			while (previousRow.First!.Value < MaxRow)
 			{
-				Console.WriteLine("Row({0}): {2}", rowIndex, foundCount, total);
-			}
-			//Console.WriteLine(string.Join(' ', previousRow));
-			//Console.ReadKey();
+				var count = FillNextRowFromPrevious(previousRow, nextRow);
 
-			foundCount = 0;
-			if (rowIndex == MaxRow) return;
+				total += count;
+				var row = nextRow.First!.Value;
+				if (row % 1000 == 0)
+					Console.WriteLine("Row({0}): {2}", row, count, total);
 
-			Parallel.ForEach(Nodes(), nodes =>
-			{
-				var (pN, nN) = nodes;
-				BigInteger left = pN.Previous?.Value ?? rowIndex;
-				BigInteger right = pN.Value;
-				nN.Value = IncrementIfFound(left + right, 2);
-			});
-
-			++rowIndex;
-
-			if (rowIndex % 2 == 0)
-			{
-				var last = previousRow.Last.Value;
-				nextRow.AddLast(last + last);
+				var swap = previousRow;
+				previousRow = nextRow;
+				nextRow = swap;
 			}
 
-			var swap = previousRow;
-			previousRow = nextRow;
-			nextRow = swap;
+			Console.WriteLine("Row({0}): {1}", MaxRow, total);
+		}
 
-			Console.WriteLine("Row({0}): {1}", rowIndex, total);
+		static long FillNextRowFromPrevious(
+			LinkedList<BigInteger> previousRow,
+			LinkedList<BigInteger> nextRow)
+		{
+			long foundCount = 0;
+			Parallel.ForEach(Nodes(previousRow, nextRow),
+				nodes =>
+				nodes.current.Value
+					= IncrementIfFound(NextRowValue(nodes.previous)));
 
-			goto loop;
+			// Both sides of the triangle.
+			foundCount += foundCount;
 
-			BigInteger IncrementIfFound(in BigInteger value, int count = 1)
+			// Even rows take the previous value and double it.
+			if (nextRow.First!.Value % 2 == 0)
+			{
+				var last = previousRow.Last!.Value;
+				nextRow.AddLast(IncrementIfFound(last + last));
+			}
+
+			return foundCount;
+
+			BigInteger IncrementIfFound(in BigInteger value)
 			{
 				if (value % SearchingFor == 0)
-				{
-					for (var i = 0; i < count; i++)
-						Interlocked.Increment(ref foundCount);
-				}
+					Interlocked.Increment(ref foundCount);
 				return value;
 			}
-
-			IEnumerable<(LinkedListNode<BigInteger> p, LinkedListNode<BigInteger> n)> Nodes()
-			{
-				var pN = previousRow.First;
-				var nN = nextRow.First;
-				yield return (pN, nN);
-
-				while ((pN = pN.Next) != null)
-				{
-					nN = nN.Next ?? nextRow.AddLast(BigInteger.Zero);
-					yield return (pN, nN);
-				}
-			}
-
 		}
+
+		static IEnumerable<(LinkedListNode<BigInteger> previous, LinkedListNode<BigInteger> current)> Nodes(
+			LinkedList<BigInteger> previousRow,
+			LinkedList<BigInteger> nextRow)
+		{
+			LinkedListNode<BigInteger>? pN = previousRow.First!;
+			LinkedListNode<BigInteger>? nN = nextRow.First!;
+
+			yield return (pN, nN);
+
+			while ((pN = pN.Next!) != null)
+			{
+				nN = nN.Next ?? nextRow.AddLast(BigInteger.Zero);
+				yield return (pN, nN);
+			}
+		}
+
+		static BigInteger NextRowValue(
+			LinkedListNode<BigInteger> previousRowNode)
+			=> previousRowNode.Value
+			 + (previousRowNode.Previous?.Value ?? BigInteger.One);
 	}
 }
